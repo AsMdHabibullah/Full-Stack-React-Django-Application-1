7ab323d8f991f5ab2bb24443e9ad4b8225a131c3
Abu Sayem Md Habibullah <37944391+AsMdHabibullah@users.noreply.github.com>
2019-04-10 00:44:34 +0800

    Update old_settings.py

--- backapp/backapp/old_settings.py
+++ backapp/backapp/old_settings.py
@@ -88,6 +88,16 @@ WSGI_APPLICATION = 'backapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'profile',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 DATABASES = {
     'default': {
